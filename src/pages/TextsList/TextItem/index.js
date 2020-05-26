daeaf045a823fa6ce864257d7825e74b945be62a
yuzzon <yuzzon@gmail.com>
2020-05-26 16:29:59 +0300

    minor fix

--- src/pages/TextsList/TextItem/index.js
+++ src/pages/TextsList/TextItem/index.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 import { Typography } from '@material-ui/core';
 import { ShortenedP, TextContainer } from './styles';
 
-const TextItem = (props) => {
+const TextItem = ({id, text, title}) => {
    return (
       <TextContainer>
-         <Typography variant="h5">{props.title}</Typography>
-         <ShortenedP>{props.text}</ShortenedP>
-         <Link to={`/text/${props.id}`}>See more</Link>
+         <Typography variant="h5">{title}</Typography>
+         <ShortenedP>{text}</ShortenedP>
+         <Link to={`/text/${id}`}>See more</Link>
       </TextContainer>
    );
 };
